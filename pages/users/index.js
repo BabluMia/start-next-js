29362250949bb389bb23bb5beceff6bf80550055
Bablu Mia <bablumiah2000@gmail.com>
2022-05-29 13:43:33 +0600

    user loaded

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
-const index = ({users}) => {
-    return (
-        <div>
-            <h3>This is from users :{users.length}</h3>
-        </div>
-    );
+const index = ({ users }) => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", width: "100vw" }}>
+      <div>
+        <h3>This is from users :{users.length}</h3>
+        {users.map((user) => (
+          <div key={user.id}>
+            <h4>
+              {user.name}
+              <Link href={`users/${user.id}`}>
+                <button>Explore</button>
+              </Link>
+            </h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default index;
 
 export async function getStaticProps(context) {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await res.json()
-    return {
-      props: {users:data}, // will be passed to the page component as props
-    }
-  }
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { users: data }, // will be passed to the page component as props
+  };
+}
---
+++ pages/users/managers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const managers = () => {
+    return (
+        <div>
+            <h3>from all managers</h3>
+        </div>
+    );
+};
+
+export default managers;
